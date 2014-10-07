10cfc00841773a1dd3bd7e83d57f4e7e44e97915
Madhur <ahuja.madhur@gmail.com>
2014-10-07 17:55:01 +0530

    updated weather conky

--- conky_start.sh
+++ conky_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-conky -p 15 -c ~/.conkyrc-core &
-conky -p 15 -c ~/.conkyrc-weather &
-conky -p 15 -c ~/.conkyrc-social
+conky -p 15 -c ~/.conky/.conkyrc-core &
+conky -p 15 -c ~/.conky/.conkyrc-weather &
+conky -p 15 -c ~/.conky/.conkyrc-social
