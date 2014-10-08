e6200cbaa5058d399afbeeef2be24ad3aaa7a347
Madhur <ahuja.madhur@gmail.com>
2014-10-09 00:57:44 +0530

    work in progress

--- scripts/weather.py
+++ scripts/weather.py
@@ -1,13 +1,13 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
-import urllib2
+import urllib
 import json
-import zlib
 from subprocess import call
 import sys
 import time
 from xml.dom import minidom
 from os.path import expanduser
+import urllib.request
 
 json_data=open(expanduser('~')+'/.conky/scripts/.passwords.json')
 data = json.load(json_data)
@@ -15,7 +15,7 @@ location_code=data['weather']['location_code']
 
 url = 'http://weather.yahooapis.com/forecastrss?w='+location_code+'&u=c'
 
-j = urllib2.urlopen(url)
+j = urllib.request.urlopen(url)
 weather_data = j.read()
 xmldoc = minidom.parseString(weather_data)
 locs = xmldoc.getElementsByTagName("yweather:location")
@@ -26,6 +26,10 @@ locs = xmldoc.getElementsByTagName("yweather:condition")
 current_condition= locs[0].attributes['text'].value
 current_code = locs[0].attributes['code'].value
 current_temp = locs[0].attributes['temp'].value
+weather_date=locs[0].attributes['date'].value
+
+locs = xmldoc.getElementsByTagName("yweather:units")
+speed_unit = locs[0].attributes['speed'].value
 
 locs = xmldoc.getElementsByTagName("yweather:wind")
 speed =  locs[0].attributes['speed'].value
@@ -38,4 +42,16 @@ locs = xmldoc.getElementsByTagName("yweather:forecast")
 for loc in locs:
 	forecast.append((loc.attributes['day'].value, loc.attributes['low'].value, loc.attributes['high'].value , loc.attributes['code'].value))
 
-print forecast
+
+print ("${font Open Sans:size=15:style=Light}%s, %s${font}" % (city, country))
+print ("${color7}${hr}${color}")
+print ("${font Open Sans:size=60:style=Light}${alignr}%s°${font}${voffset -20}" %(current_temp))
+print ("${image ~/.conky/.conky-google-now/%s.png -p 0,65 -s 60x60}" %(current_code))
+print ("%s" %(current_condition))
+print ("${image ~/.conky/.conky-google-now/wind.png -p 0,135 -s 15x15}${goto 35}%s %s" %(speed, speed_unit))
+print ("${image ~/.conky/.conky-google-now/humidity.png -p 0,155 -s 15x15}${goto 35}%s %s" %(humidity,"%"))
+print ("${image ~/.conky/.conky-google-now/%s.png -p 0,185 -s 30x30}${image ~/.conky/.conky-google-now/%s.png -p 130,185 -s 30x30}${voffset -10}" %(forecast[0][3], forecast[1][3]))
+print ("${goto 60}%s ${goto 190} %s" %(forecast[0][0].upper(), forecast[1][0].upper()))
+print ("${goto 60}%s°${color6}%s°${color}${goto 190}%s°${color6}%s°${color}${voffset 15}" %(forecast[0][2], forecast[0][1], forecast[1][2], forecast[1][1]))
+print ("${font :size=6}${color7}Updated: ${alignr}${color white}%s" %(time.strftime("%I:%M")))
+print ("${color7}Published: ${alignr}${color white}%s" %(weather_date))
