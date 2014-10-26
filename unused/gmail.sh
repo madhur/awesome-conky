e87b913ee51da0eaf65ef4945ca796b8a9ec7922
Madhur <ahuja.madhur@gmail.com>
2014-10-26 13:09:27 +0530

    removed

--- unused/gmail.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#curl -u ahuja.madhur:cmu@word10 --silent "https://mail.google.com/mail/feed/atom/unread" | tr -d '\n' | awk -F '<entry>' '{for (i=2; i<=NF; i++) {print $i}}' | sed -n "s/<title>\(.*\)<\/title.*name>\(.*\)<\/name>.*/\${color1}\2 \${goto 160} \${color2}\1/p"
-curl -u ahuja.madhur:cmu@word10 --silent "https://mail.google.com/mail/feed/atom/unread" | tr -d '\n' | awk -F '<entry>' '{for (i=2; i<=NF; i++) {print $i}}' | sed -n "s/<title>\(.*\)<\/title.*name>\(.*\)<\/name>.*/\${color1}\2 \${goto 160} \${color2}\1/p"
-
-notify-send "Gmail Updated"
